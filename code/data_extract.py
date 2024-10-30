f81cc8f2ae58f7e753bf835238b3033b48ad88c0
Misael <misaelarturo@gmail.com>
2024-10-31 00:16:39 +0800

    Adds parameters to data extraction

--- code/data_extract.py
+++ code/data_extract.py
@@ -1,19 +1,23 @@
+from pathlib import Path
+
 import nibabel as nib
 
-FILE_NAME = 'ima_REP1_150BR_32_sl_20080130101823_23.nii'
+POPULATION = 'young'
+SUBJECT = 'y03'
+FILE_NAME = 'ima_T1_MPRAGE_sag_1.25_hippo_20080312142213_13.nii'
 
-img = nib.load('../data/rawdata/young/y01/' + FILE_NAME)
+def extract_data(file_name):
+    img = nib.load(file_name)
+    voxel_dims = img.header.get_zooms()
+    data = img.get_fdata()
+    matrix_size = data.shape[:2]
+    slice_thickness = voxel_dims[2]
+    tr = img.header['pixdim'][4]
+    print("Voxel dimensions:", voxel_dims)
+    print("Matrix size:", matrix_size)
+    print("Slice thickness:", slice_thickness)
+    print("Repetition Time (TR):", tr)
 
-voxel_dims = img.header.get_zooms()
-print("Voxel dimensions:", voxel_dims)
-
-data = img.get_fdata()
-matrix_size = data.shape[:2]  # Taking the first two dimensions
-print("Matrix size:", matrix_size)
-
-slice_thickness = voxel_dims[2]
-print("Slice thickness:", slice_thickness)
-
-tr = img.header['pixdim'][4]
-print("Repetition Time (TR):", tr)
-
+if __name__ == '__main__':
+    print('Data for subject', SUBJECT, 'in population', POPULATION)
+    extract_data(Path('../data/rawdata') / POPULATION / SUBJECT /  FILE_NAME)
